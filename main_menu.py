479797fde8a5e3323b4f94ab429a76efb58f606e
Kim-jeongmin <jmkimmessi@gmail.com>
2022-09-06 00:56:59 +0900

    add wielding sound

--- main_menu.py
+++ main_menu.py
@@ -13,9 +13,6 @@ for i in range(0, 10):
     title_images.append(pygame.image.load(title_images_base_path + str(i) + '.png'))
 
 
-
-
-
 def draw_text(text, size, color, x, y):
     font = pygame.font.Font(None, 40)
     text_surface = font.render(text, True, color)
