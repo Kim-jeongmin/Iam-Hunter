21a8b3eb843b63b5cd9a430ff4859bfa5421cadc
Kim-jeongmin <jmkimmessi@gmail.com>
2022-09-05 20:13:49 +0900

    delete code

--- data/engine.py
+++ data/engine.py
@@ -57,13 +57,7 @@ class bullet(map_obj):
     def get_rect(self):
         return pygame.Rect(self.loc[0], self.loc[1], 1, 1)
 
-class arrow(map_obj):
-    def __init__(self, loc, flip):
-        super().__init__(loc)
-        self.flip = flip
 
-    def get_rect(self):
-        return pygame.Rect(self.loc[0], self.loc[1], 12, 1)
     
 
 # 2d physics object
@@ -112,38 +106,6 @@ class physics_obj(object):
             self.y = self.rect.y
         return collision_types
 
-# 3d collision detection
-# todo: add 3d physics-based movement
-
-# class cuboid(object):
-    
-#     def __init__(self,x,y,z,x_size,y_size,z_size):
-#         self.x = x
-#         self.y = y
-#         self.z = z
-#         self.x_size = x_size
-#         self.y_size = y_size
-#         self.z_size = z_size
-        
-#     def set_pos(self,x,y,z):
-#         self.x = x
-#         self.y = y
-#         self.z = z
-        
-#     def collidecuboid(self,cuboid_2):
-#         cuboid_1_xy = pygame.Rect(self.x,self.y,self.x_size,self.y_size)
-#         cuboid_1_yz = pygame.Rect(self.y,self.z,self.y_size,self.z_size)
-#         cuboid_2_xy = pygame.Rect(cuboid_2.x,cuboid_2.y,cuboid_2.x_size,cuboid_2.y_size)
-#         cuboid_2_yz = pygame.Rect(cuboid_2.y,cuboid_2.z,cuboid_2.y_size,cuboid_2.z_size)
-#         if (cuboid_1_xy.colliderect(cuboid_2_xy)) and (cuboid_1_yz.colliderect(cuboid_2_yz)):
-#             return True
-#         else:
-#             return False
-
-# entity stuff
-
-# def simple_entity(x,y,e_type):
-#     return entity(x,y,1,1,e_type)
 
 def flip(img,boolean=True):
     return pygame.transform.flip(img,boolean,False)
@@ -213,15 +175,6 @@ class entity(object):
             self.set_animation_tags(anim[1])
             self.animation_frame = 0
 
-    # def get_entity_angle(self, entity_2):
-    #     x1 = self.x+int(self.size_x/2)
-    #     y1 = self.y+int(self.size_y/2)
-    #     x2 = entity_2.x+int(entity_2.size_x/2)
-    #     y2 = entity_2.y+int(entity_2.size_y/2)
-    #     angle = math.atan((y2-y1)/(x2-x1))
-    #     if x2 < x1:
-    #         angle += math.pi
-    #     return angle
 
     def get_center(self):
         x = self.x+int(self.size_x/2)
@@ -259,29 +212,7 @@ class entity(object):
                     self.animation_frame = len(self.animation)-1
                     
  
-    # def get_current_img(self):
-    #     if self.animation == None:
-    #         if self.image != None:
-    #             return flip(self.image,self.flip)
-    #         else:
-    #             return None
-    #     else:
-    #         return flip(animation_database[self.animation[self.animation_frame]],self.flip)
 
-    # def get_drawn_img(self):
-    #     image_to_render = None
-    #     if self.animation == None:
-    #         if self.image != None:
-    #             image_to_render = flip(self.image,self.flip).copy()
-    #     else:
-    #         image_to_render = flip(animation_database[self.animation[self.animation_frame]],self.flip).copy()
-    #     if image_to_render != None:
-    #         center_x = image_to_render.get_width()/2
-    #         center_y = image_to_render.get_height()/2
-    #         image_to_render = pygame.transform.rotate(image_to_render,self.rotation)
-    #         if self.alpha != None:
-    #             image_to_render.set_alpha(self.alpha)
-    #         return image_to_render, center_x, center_y
  
     def display(self,surface,scroll):
         image_to_render = None
@@ -348,76 +279,4 @@ def load_animations(path):
         if entity_type not in animation_higher_database:
             animation_higher_database[entity_type] = {}
         animation_higher_database[entity_type][animation_id] = [anim.copy(),tags]
-
-# particles
-
-# def particle_file_sort(l):
-#     l2 = []
-#     for obj in l:
-#         l2.append(int(obj[:-4]))
-#     l2.sort()
-#     l3 = []
-#     for obj in l2:
-#         l3.append(str(obj) + '.png')
-#     return l3
-
-# global particle_images
-# particle_images = {}
-
-# def load_particle_images(path):
-#     global particle_images, e_colorkey
-#     file_list = os.listdir(path)
-#     for folder in file_list:
-#         try:
-#             img_list = os.listdir(path + '/' + folder)
-#             img_list = particle_file_sort(img_list)
-#             images = []
-#             for img in img_list:
-#                 images.append(pygame.image.load(path + '/' + folder + '/' + img).convert())
-#             for img in images:
-#                 img.set_colorkey(e_colorkey)
-#             particle_images[folder] = images.copy()
-#         except:
-#             pass
-
-# class particle(object):
-
-#     def __init__(self,x,y,particle_type,motion,decay_rate,start_frame,custom_color=None):
-#         self.x = x
-#         self.y = y
-#         self.type = particle_type
-#         self.motion = motion
-#         self.decay_rate = decay_rate
-#         self.color = custom_color
-#         self.frame = start_frame
-
-#     def draw(self,surface,scroll):
-#         global particle_images
-#         if self.frame > len(particle_images[self.type])-1:
-#             self.frame = len(particle_images[self.type])-1
-#         if self.color == None:
-#             blit_center(surface,particle_images[self.type][int(self.frame)],(self.x-scroll[0],self.y-scroll[1]))
-#         else:
-#             blit_center(surface,swap_color(particle_images[self.type][int(self.frame)],(255,255,255),self.color),(self.x-scroll[0],self.y-scroll[1]))
-
-#     def update(self):
-#         self.frame += self.decay_rate
-#         running = True
-#         if self.frame > len(particle_images[self.type])-1:
-#             running = False
-#         self.x += self.motion[0]
-#         self.y += self.motion[1]
-#         return running
-        
-
-# # other useful functions
-
-# def swap_color(img,old_c,new_c):
-#     global e_colorkey
-#     img.set_colorkey(old_c)
-#     surf = img.copy()
-#     surf.fill(new_c)
-#     surf.blit(img,(0,0))
-#     surf.set_colorkey(e_colorkey)
-#     return surf
 
